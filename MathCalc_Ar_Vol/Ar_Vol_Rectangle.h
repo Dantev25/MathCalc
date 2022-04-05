cade7d435c20606c493830a34e8485290f006499
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-05 14:54:48 +0400

    Added area and perimeter calculations for rectangle

---
+++ MathCalc_Ar_Vol/Ar_Vol_Rectangle.h
@@ -0,0 +1,23 @@
+#ifndef AR_VOL_RECTANGLE_H_INCLUDED
+#define AR_VOL_RECTANGLE_H_INCLUDED
+
+float rectangle()
+{
+    float l, b, area, perimeter;
+
+    printf("Enter the length and breadth of the rectangle:\n");
+    printf("Length = ");
+    scanf("%f",&l);
+    printf("Breadth = ");
+    scanf("%f",&b);
+
+    area = l * b;
+    perimeter = (l + b) * 2;
+
+    printf("\nArea of rectangle = %.2f\n",area);
+    printf("perimeter of rectangle = %.2f\n",perimeter);
+
+    return 0;
+}
+
+#endif
