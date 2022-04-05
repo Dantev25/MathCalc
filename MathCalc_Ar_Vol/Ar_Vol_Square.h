ad5c7a0279d0e321d85176b07409974ab07fd81b
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-05 14:29:00 +0400

    Added area and perimeter calculations for square

---
+++ MathCalc_Ar_Vol/Ar_Vol_Square.h
@@ -0,0 +1,20 @@
+#ifndef AR_VOL_SQUARE_H_INCLUDED
+#define AR_VOL_SQUARE_H_INCLUDED
+
+float square()
+{
+    float x, area, perimeter;
+
+    printf("Enter the length of the side of the square: ");
+    scanf("%f",&x);
+
+    area = x*x;
+    perimeter = x*4;
+
+    printf("\nArea of square = %.2f\n",area);
+    printf("perimeter of square = %.2f\n",perimeter);
+
+    return 0;
+}
+
+#endif
