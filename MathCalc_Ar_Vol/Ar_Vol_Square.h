d46ceca04c321df15b3cc22c4ec8fd6a8a8076c6
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-07 12:11:46 +0400

    Added square details to .txt file.

--- MathCalc_Ar_Vol/Ar_Vol_Square.h
+++ MathCalc_Ar_Vol/Ar_Vol_Square.h
@@ -5,6 +5,8 @@ float square()
 {
     float x, area, perimeter;
 
+    FILE *fptr;
+
     printf("Enter the length of the side of the square: ");
     scanf("%f",&x);
 
@@ -18,6 +20,19 @@ float square()
 
     printf("\nArea of square = %.2f\n",area);
     printf("perimeter of square = %.2f\n",perimeter);
+
+    fptr = (fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
+    }
+
+    fprintf(fptr,"Shape = square\n");
+    fprintf(fptr,"length of side = %.2f\n",x);
+    fprintf(fptr,"Area = %.2f\nPerimeter = %.2f\n\n",area,perimeter);
+
+    fclose(fptr);
 
     return 0;
 }
