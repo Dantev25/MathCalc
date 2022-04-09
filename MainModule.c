6c331cbfd380ec31130b73552c112f2955312f41
Dantev25 <oomargingo@gmail.com>
2022-04-09 21:50:40 +0400

    testing quad()

--- MainModule.c
+++ MainModule.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"MathCalc_Ar_Vol\Ar_Vol_Square.h"
-
+#include"MathCalc_Quadratic\Quadratic_Addition.h"
 void main()
 {
-    square();
+    QuadAdd();
 }
