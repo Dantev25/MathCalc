8b6189c4ff976e35cfe2426a832e7d0d45fdf783
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-10 15:55:34 +0400

    updated codes for cube and cuboid

--- MathCalc_Ar_Vol/Ar_Vol_Cube.h
+++ MathCalc_Ar_Vol/Ar_Vol_Cube.h
@@ -1,9 +1,14 @@
 #ifndef AR_VOL_CUBE_H_INCLUDED
 #define AR_VOL_CUBE_H_INCLUDED
+#include<time.h>
 
 float cube()
 {
     float x, sa, vol;
+    time_t t;   // not a primitive datatype
+    time(&t);
+
+    FILE *fptr;
 
     printf("Enter the length of the side of the cube: ");
     scanf("%f",&x);
@@ -18,6 +23,20 @@ float cube()
 
     printf("\nSurface area of cube = %.2f\n",sa);
     printf("Volume of cube = %.2f\n",vol);
+
+    fptr = (fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
+    }
+
+    fprintf(fptr,"executed on: %s",ctime(&t));
+    fprintf(fptr,"Shape = cube\n");
+    fprintf(fptr,"length of side = %.2f\n",x);
+    fprintf(fptr,"Surface area = %.2f\nVolume = %.2f\n\n",sa,vol);
+
+    fclose(fptr);
 
     return 0;
 }
