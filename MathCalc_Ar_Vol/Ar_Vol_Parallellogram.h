e66a244ee6b455c040026070895ad304dbc541b1
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-10 15:35:28 +0400

    Added codes for parallellogram to write in .txt file and include time.

--- MathCalc_Ar_Vol/Ar_Vol_Parallellogram.h
+++ MathCalc_Ar_Vol/Ar_Vol_Parallellogram.h
@@ -1,9 +1,14 @@
 #ifndef AR_VOL_PARALLELLOGRAM_H_INCLUDED
 #define AR_VOL_PARALLELLOGRAM_H_INCLUDED
+#include<time.h>
 
 float parallellogram()
 {
     float side, base, height, area, perimeter;
+    time_t t;   // not a primitive datatype
+    time(&t);
+
+    FILE *fptr;
 
     printf("Enter the length of the side and base, and height of the parrallellogram:\n");
     printf("side = ");
@@ -14,13 +19,21 @@ float parallellogram()
     scanf("%f",&height);
 
     while((side <= 0)||(base <= 0)||(height <= 0)){
-        printf("\nCan only input positive integers!\nInput again!\n");
-        printf("side = ");
-        scanf("%f",&side);
-        printf("base = ");
-        scanf("%f",&base);
-        printf("height = ");
-        scanf("%f",&height);
+        if(side <=0){
+           printf("\nCan only input positive integer for side!\nInput again!\n");
+           printf("side = ");
+           scanf("%f",&side); 
+        }
+        else if(base <= 0){
+            printf("\nCan only input positive integer for base!\nInput again!\n");
+            printf("base = ");
+            scanf("%f",&base);
+        }
+        else if(height <= 0){
+            printf("\nCan only input positive integer for height!\nInput again!\n");
+            printf("height = ");
+            scanf("%f",&height);
+        }
     }
 
     area = base * height;
@@ -28,6 +41,20 @@ float parallellogram()
 
     printf("\nArea of parallellogram = %.2f\n",area);
     printf("perimeter of parallellogram = %.2f\n",perimeter);
+
+    fptr = (fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
+    }
+
+    fprintf(fptr,"executed on: %s",ctime(&t));
+    fprintf(fptr,"Shape = parallellogram\n");
+    fprintf(fptr,"side = %.2f, base = %.2f, height = %.2f\n",side,base,height);
+    fprintf(fptr,"Area = %.2f\nPerimeter = %.2f\n\n",area,perimeter);
+
+    fclose(fptr);
 
     return 0;
 }
