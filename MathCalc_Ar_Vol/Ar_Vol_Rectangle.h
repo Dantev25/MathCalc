d71cbe8fc12b3e447a4bacbe7285912d4094bf1e
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-10 14:49:59 +0400

    Added codes for rectangle to write in .txt file and to include time.

--- MathCalc_Ar_Vol/Ar_Vol_Rectangle.h
+++ MathCalc_Ar_Vol/Ar_Vol_Rectangle.h
@@ -1,9 +1,14 @@
 #ifndef AR_VOL_RECTANGLE_H_INCLUDED
 #define AR_VOL_RECTANGLE_H_INCLUDED
+#include<time.h>
 
 float rectangle()
 {
     float l, b, area, perimeter;
+    time_t t;   // not a primitive datatype
+    time(&t);
+
+    FILE *fptr;
 
     printf("Enter the length and breadth of the rectangle:\n");
     printf("Length = ");
@@ -24,6 +29,20 @@ float rectangle()
 
     printf("\nArea of rectangle = %.2f\n",area);
     printf("perimeter of rectangle = %.2f\n",perimeter);
+
+    fptr = (fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
+    }
+
+    fprintf(fptr,"executed on: %s",ctime(&t));
+    fprintf(fptr,"Shape = rectangle\n");
+    fprintf(fptr,"length = %.2f, breadth = %.2f\n",l,b);
+    fprintf(fptr,"Area = %.2f\nPerimeter = %.2f\n\n",area,perimeter);
+
+    fclose(fptr);
 
     return 0;
 }
