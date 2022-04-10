1ccf6a7bde754eb80d69ff8649f145afd4197e74
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-10 15:36:43 +0400

    Added library to Matrix Scalar Multiplication.

--- MathCalc_Matrix/matrixScalarMultiplication.h
+++ MathCalc_Matrix/matrixScalarMultiplication.h
@@ -1,5 +1,6 @@
 #ifndef MATRIX_SCALAR_MULTIPLICATION_H_INCLUDED
 #define MATRIX_SCALAR_MULTIPLICATION_INCLUDED
+#include<stdio.h>
 #include<math.h>
 
 void matrixScalarMultiply(int array[10][10], int scalar, int rows, int colums){
@@ -14,16 +15,16 @@ void matrixScalarMultiply(int array[10][10], int scalar, int rows, int colums){
     }
 
     fprintf(fptr,"Matrix Operation = Scalar Multiplication\n");
-    fprintf("\nOutput Matrix:\n");
+    fprintf(fptr,"\nOutput Matrix:\n");
 
     for (i = 0; i < rows; i++){
         for (j = 0; j < colums; j++){
             scaM[i][j] = scalar * array[i][j];
             printf("%d\t", scaM[i][j]);
-            fprintf("%d\t", scaM[i][j]);
+            fprintf(fptr, "%d\t", scaM[i][j]);
         }
         printf("\n");
-        fprintf("\n");
+        fprintf(fptr, "\n");
     }
     fclose(fptr);
 
