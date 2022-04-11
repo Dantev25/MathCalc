b89950ee6def566923effa95d726a59ba8a71b01
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-11 11:31:10 +0400

    Modified Matrix Subtraction Code and tested in MainTest.

--- MainTest.c
+++ MainTest.c
@@ -1,29 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-//#include"MathCalc_Ar_Vol\Ar_Vol_Cone.h"
-
-#include"MathCalc_Matrix\matrixMultiplication.h"
+#include"MathCalc_Matrix\matrixSubtraction.h"
 
 int main(void){
 
-    int i, j, k; //used in for loops
+     int i, j, k; //used in for loops
 
     int matrixA[10][10]; // initialized at 10 just to have it initialized
     int matrixB[10][10];
     int rowA, colA;
     int rowB, colB;
+    int sub = -1;
 
-            //when mulptiplying arrays matrixA colum # has to equal matrixB row #
-            printf("\nEnter the #rows and #cols for matrix A: ");
+
+              printf("\nEnter the #rows and #cols for matrix A: ");
             scanf("%d%d", &rowA, &colA);
 
             printf("Enter the #rows and #cols for matrix B: ");
             scanf("%d%d", &rowB, &colB);
 
-            // Column of first matrix should be equal to column of second matrix and
-            while (colA != rowB)
-            {
-                printf("\n\nError! column of first matrix not equal to row of second.\n\n");
+            while ((rowA != rowB) && (colA != colB)){
+                printf("\nMatrices must be the same size\n");
                 printf("\nEnter the #rows and #cols for matrix A: ");
                 scanf("%d%d", &rowA, &colA);
 
@@ -31,20 +28,19 @@ int main(void){
                 scanf("%d%d", &rowB, &colB);
             }
 
-            // Storing elements of first matrix.
             printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
             readMatrix(matrixA, rowA, colA);
             printf("\n\t\tMatrix A\n\n");
             printMatrix(matrixA, rowA, colA);
 
-            // Storing elements of second matrix.
             printf("\n\tEnter elements of Matrix B a %d x %d matrix.\n", rowB, colB); // with the %d we remember the user the dimentions of the array
             readMatrix(matrixB, rowB, colB);
-            printf("\n\t\tMatrix A\n\n");
+            printf("\n\t\tMatrix B\n\n");
             printMatrix(matrixB, rowB, colB);
 
-            //multiplying arrays
-            matrixMultiply(matrixA, matrixB, rowA, colA, colB);
+            printf("\nThe difference between matrixA - matrixB is : \n");
+            matrixSub(matrixA, matrixB, rowA, colA, sub);
+            
 
     return 0;
 }
--- MathCalc_Matrix/matrixSubtraction.h
+++ MathCalc_Matrix/matrixSubtraction.h
@@ -2,12 +2,35 @@
 #define MATRIX_SUBTRACTION_INCLUDED
 #include<math.h>
 
+//User Defined Function Definition
+void readMatrix(int array[10][10], int rows, int colums){
+    int i, j;
+    for (i = 0; i < rows; i++){
+        printf("\t%d entries for row %d: ", colums, i + 1);
+        for (j = 0; j < colums; j++){
+            scanf("%d", &array[i][j]);
+        }
+    }
+
+    return;
+}
+
+void printMatrix(int array[10][10], int rows, int colums){
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < colums; j++){
+            printf("\t%d", array[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 
 void matrixSub(int arrayone[10][10], int arraytwo[10][10], int rows, int colums, int mul){
     int i, j;
     int sumM[10][10];
     int scaM[10][10];
-    FILE *fptr;
     for (i = 0; i < rows; i++){
         for (j = 0; j < colums; j++){
             scaM[i][j] = mul * arraytwo[i][j];
@@ -22,30 +45,6 @@ void matrixSub(int arrayone[10][10], int arraytwo[10][10], int rows, int colums,
         }
         printf("\n");
     }
-
-    fptr = (fopen("SE_MATRIX/matrixLog.txt","w"));
-    
-    if(fptr==NULL){
-        printf("Error!");
-        exit(1);
-    }
-
-    fprintf(fptr,"Matrix Operation = Subtraction\n");
-    fprintf("\nOutput Matrix:\n\n");
-
-      for (i = 0; i < rows; i++){
-        for (j = 0; j < colums; j++){
-            scaM[i][j] = mul * arraytwo[i][j];
-            }
-        }
-
-
-    for (i = 0; i < rows; i++){
-        for (j = 0; j < colums; j++){
-            sumM[i][j] = arrayone[i][j] + scaM[i][j];
-            fprintf("\t%d", sumM[i][j]);
-        }
-        fprintf("\n");
-    }
 }
+
 #endif
