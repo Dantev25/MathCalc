0a4b8372de407326af975df3908fcb9fc65a5d59
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-11 23:26:03 +0400

    updated rhombus code

--- MathCalc_Ar_Vol/Ar_Vol_Rhombus.h
+++ MathCalc_Ar_Vol/Ar_Vol_Rhombus.h
@@ -1,11 +1,83 @@
 #ifndef AR_VOL_RHOMBUS_H_INCLUDED
 #define AR_VOL_RHOMBUS_H_INCLUDED
+#include<math.h>
+#include<time.h>
 
 float rhombus()
 {
-    float side, d1, d2, area, perimeter;
+    float side, x, d1, d2, area, perimeter, a;
+    int choice;
 
-    printf("Enter the length of the side and the two diagonals of the rhombus:\n");
+    printf("By which method would you like to carry the operations for rhombus:\n");
+    printf("1. By using angle\n2. By using diagonal");
+    printf("\n\nInput your choice: ");
+    scanf("%d",&choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("Input the length of the side of the rhombus: ");
+        scanf("%f",&side);
+
+        while(side <= 0){
+        printf("\nCan only input positive integer for length of the side!\nInput again: ");
+        scanf("%f",&side);
+        }
+
+        printf("Input value of the angle (in radians): ");
+        scanf("%f",&x);
+
+        while((x <= 0)||(x >= M_PI)){
+            printf("Angle should have a value between 0 and pi!\nInput again: ");
+            scanf("%f",&x);
+        }
+
+        a = pow(side,2);
+        d1 = sqrt(a + a - (2 * side * side * cos(x)));
+        d2 = sqrt((4 * a) - (pow(d1,2)));
+
+        printf("\nCalculated diagonal 1 = %.2f\n",d1);
+        printf("Calculated diagonal 2 = %.2f\n",d2);
+
+        area = (d1 * d2)/2;
+        perimeter = side * 4;
+
+        printf("\nArea of rhombus = %.2f\n",area);
+        printf("perimeter of rhombus = %.2f\n",perimeter);
+        break;
+
+    case 2:
+        printf("Input the length of the side of the rhombus: ");
+        scanf("%f",&side);
+
+        while(side <= 0){
+        printf("\nCan only input positive integer for length of the side!\nInput again: ");
+        scanf("%f",&side);
+        }
+
+        printf("Input length of diagonal 1 of the rhombus: ");
+        scanf("%f",&d1);
+
+        while(d1 <= 0){
+        printf("\nCan only input positive integer for length of diagonal!\nInput again: ");
+        scanf("%f",&d1);
+        }
+
+        d2 = sqrt((4 * pow(side,2)) - (pow(d1,2)));
+        printf("\nCalculated diagonal 2 = %.2f\n",d2);
+
+        area = (d1 * d2)/2;
+        perimeter = side * 4;
+
+        printf("\nArea of rhombus = %.2f\n",area);
+        printf("perimeter of rhombus = %.2f\n",perimeter);
+        break;
+    
+    default:
+        break;
+    }
+
+   /* printf("Enter the length of the side and the two diagonals of the rhombus:\n");
     printf("side = ");
     scanf("%f",&side);
     printf("diagonal 1 = ");
@@ -27,7 +99,7 @@ float rhombus()
     perimeter = side * 4;
 
     printf("\nArea of rhombus = %.2f\n",area);
-    printf("perimeter of rhombus = %.2f\n",perimeter);
+    printf("perimeter of rhombus = %.2f\n",perimeter);*/
 
     return 0;
 }
