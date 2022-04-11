542996dd66a0f0e78e13c26add1e258aa36a89b1
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-11 11:42:17 +0400

    Modifies Matrix Scalar Multiplication Code and tested in MainTest.

--- MainTest.c
+++ MainTest.c
@@ -1,47 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"MathCalc_Matrix\matrixAddition.h"
+#include"MathCalc_Matrix\matrixScalarMultiplication.h"
 
-int main(void){
 
-     int i, j, k; //used in for loops
+int main(){
+
+    int i, j, k; //used in for loops
 
     int matrixA[10][10]; // initialized at 10 just to have it initialized
     int matrixB[10][10];
     int rowA, colA;
     int rowB, colB;
-    int add = 1;
+    int scalar = 0;
+
+
+
+            printf("\nEnter the scalar: ");
+            scanf("%d", &scalar);
+            printf("\nThe scalar is: %d ", scalar);
+
 
             printf("\nEnter the #rows and #cols for matrix A: ");
             scanf("%d%d", &rowA, &colA);
-
-            printf("Enter the #rows and #cols for matrix B: ");
-            scanf("%d%d", &rowB, &colB);
-
-            while ((rowA != rowB) && (colA != colB)){
-                printf("\nMatrices must be the same size\n");
-                printf("\nEnter the #rows and #cols for matrix A: ");
-                scanf("%d%d", &rowA, &colA);
-
-                printf("Enter the #rows and #cols for matrix B: ");
-                scanf("%d%d", &rowB, &colB);
-
-            }
 
             printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
             readMatrix(matrixA, rowA, colA);
             printf("\n\t\tMatrix A\n\n");
             printMatrix(matrixA, rowA, colA);
 
+            printf("\nThe scalar multiplication between matrixA * %d is: \n", scalar);
+            matrixScalarMultiply(matrixA, scalar, rowA, colA);
 
-            printf("\n\tEnter elements of Matrix B a %d x %d matrix.\n", rowB, colB); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixB, rowB, colB);
-            printf("\n\t\tMatrix B\n\n");
-            printMatrix(matrixB, rowB, colB);
-
-
-            printf("\nThe Sum of matrixA + matrixB is : \n");
-            matrixAdd(matrixA, matrixB, rowA, colA, add);
 
     return 0;
 }
