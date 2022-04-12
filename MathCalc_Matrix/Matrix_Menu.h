6c60fa20ebf093a0b6f6d0e4aa469c400ebcce33
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-12 18:58:03 +0400

    Modified Matrix Menu and corrected Matrix_Determinant.h

--- MathCalc_Matrix/Matrix_Menu.h
+++ MathCalc_Matrix/Matrix_Menu.h
@@ -2,165 +2,90 @@
 #define MATRIX_MENU_H_INCLUDED
 
 #include"MathCalc_Matrix\Matrix_Addition.h"
+#include"MathCalc_Matrix\Matrix_Determinant.h"
+#include"MathCalc_Matrix\Matrix_Inverse.h"
 #include"MathCalc_Matrix\Matrix_Multiplication.h"
 #include"MathCalc_Matrix\Matrix_ScalarMultiplication.h"
 #include"MathCalc_Matrix\Matrix_Subtraction.h"
 #include"MathCalc_Matrix\Matrix_Transpose.h"
 
+#include<stdio.h>
+#include<ctype.h>
+#include<math.h>
+#include<stdlib.h>
 
-void matrixMenu()
+int matrixMenu()
 {
-    int i, j, k; //used in for loops
-
-    int matrixA[10][10]; // initialized at 10 just to have it initialized
-    int matrixB[10][10];
-    int rowA, colA;
-    int rowB, colB;
-    int operation;//used in swtich statements
+    int operation;
     char again = 'Y';
-    int scalar = 0;
-    int add = 1;
-    int sub = -1;
 
-    while (again == 'Y'){
+    while (again == 'Y')
+    {
 
-
-        //this is the operation menu just type A, B, C or D to calculate
-        printf("\nOperation Menu\n");
-        printf("\t1. Addition\n");
-        printf("\t2. Subtraction\n");
-        printf("\t3. Scalar Multiplication\n");
-        printf("\t4. Multiplication of two matrices\n");
+        printf("\nMatrix Operation Menu\n");
+        printf("\n\nPlease type in the number associated to the operation you wish to perform.\n");
+        printf("\t1- Addition\n");
+        printf("\t2- Subtraction\n");
+        printf("\t3- Scalar Multiplication\n");
+        printf("\t4- Multiplication of two matrices\n");
+        printf("\t5- Transpose\n");
+        printf("\t6- Determinant\n");
+        printf("\t7- Inverse\n");
         printf("Enter your choice: ");
         scanf(" %d", &operation);
 
 
         switch (operation){
 
-        case 1:
-            printf("\nEnter the #rows and #cols for matrix A: ");
-            scanf("%d%d", &rowA, &colA);
-
-            printf("Enter the #rows and #cols for matrix B: ");
-            scanf("%d%d", &rowB, &colB);
-
-            while ((rowA != rowB) && (colA != colB)){
-                printf("\nMatrices must be the same size\n");
-                printf("\nEnter the #rows and #cols for matrix A: ");
-                scanf("%d%d", &rowA, &colA);
-
-                printf("Enter the #rows and #cols for matrix B: ");
-                scanf("%d%d", &rowB, &colB);
-
-            }
-
-
-            printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixA, rowA, colA);
-            printf("\n\t\tMatrix A\n\n");
-            printMatrix(matrixA, rowA, colA);
-
-
-            printf("\n\tEnter elements of Matrix B a %d x %d matrix.\n", rowB, colB); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixB, rowB, colB);
-            printf("\n\t\tMatrix B\n\n");
-            printMatrix(matrixB, rowB, colB);
-
-
-            printf("\nThe Sum of matrixA + matrixB is : \n");
-            matrixAdd(matrixA, matrixB, rowA, colA, add);
+        case 1: 
+        
+            addition();
 
             break;
 
         case 2:
 
-            printf("\nEnter the #rows and #cols for matrix A: ");
-            scanf("%d%d", &rowA, &colA);
-
-            printf("Enter the #rows and #cols for matrix B: ");
-            scanf("%d%d", &rowB, &colB);
-
-            while ((rowA != rowB) && (colA != colB)){
-                printf("\nMatrices must be the same size\n");
-                printf("\nEnter the #rows and #cols for matrix A: ");
-                scanf("%d%d", &rowA, &colA);
-
-                printf("Enter the #rows and #cols for matrix B: ");
-                scanf("%d%d", &rowB, &colB);
-            }
-
-            printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixA, rowA, colA);
-            printf("\n\t\tMatrix A\n\n");
-            printMatrix(matrixA, rowA, colA);
-
-            printf("\n\tEnter elements of Matrix B a %d x %d matrix.\n", rowB, colB); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixB, rowB, colB);
-            printf("\n\t\tMatrix B\n\n");
-            printMatrix(matrixB, rowB, colB);
-
-            printf("\nThe difference between matrixA - matrixB is : \n");
-            matrixSub(matrixA, matrixB, rowA, colA, sub);
+            subtraction();
             break;
 
         case 3:
 
-            printf("\nEnter the scalar: ");
-            scanf("%d", &scalar);
-            printf("\nThe scalar is: %d ", scalar);
-
-
-            printf("\nEnter the #rows and #cols for matrix A: ");
-            scanf("%d%d", &rowA, &colA);
-
-            printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixA, rowA, colA);
-            printf("\n\t\tMatrix A\n\n");
-            printMatrix(matrixA, rowA, colA);
-
-            printf("\nThe scalar multiplication between matrixA * %d is: \n", scalar);
-            matrixScalarMultiply(matrixA, scalar, rowA, colA);
+            scalarmultiplication();
 
             break;
 
         case 4:
-            //when mulptiplying arrays matrixA colum # has to equal matrixB row #
-            printf("\nEnter the #rows and #cols for matrix A: ");
-            scanf("%d%d", &rowA, &colA);
+            
+            multiplication();
 
-            printf("Enter the #rows and #cols for matrix B: ");
-            scanf("%d%d", &rowB, &colB);
+            break;
 
-            // Column of first matrix should be equal to column of second matrix and
-            while (colA != rowB)
-            {
-                printf("\n\nError! column of first matrix not equal to row of second.\n\n");
-                printf("\nEnter the #rows and #cols for matrix A: ");
-                scanf("%d%d", &rowA, &colA);
+        case 5:
+            
+            transpose();
 
-                printf("Enter the #rows and #cols for matrix B: ");
-                scanf("%d%d", &rowB, &colB);
-            }
+            break;
 
-            // Storing elements of first matrix.
-            printf("\n\tEnter elements of Matrix A a %d x %d matrix.\n", rowA, colA); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixA, rowA, colA);
-            printf("\n\t\tMatrix A\n\n");
-            printMatrix(matrixA, rowA, colA);
+        case 6:
+            
+            determinant();
 
-            // Storing elements of second matrix.
-            printf("\n\tEnter elements of Matrix B a %d x %d matrix.\n", rowB, colB); // with the %d we remember the user the dimentions of the array
-            readMatrix(matrixB, rowB, colB);
-            printf("\n\t\tMatrix A\n\n");
-            printMatrix(matrixB, rowB, colB);
+            break;
 
-            //multiplyng arrays
-            matrixMultiply(matrixA, matrixB, rowA, colA, colB);
+        case 7:
+            
+            inverse();
+
+            break;
+
+         case 8:
+            
+            //log();
 
             break;
 
         default:
-            printf("\nIncorrect option! Please choose a number 1-4.");
+            printf("\nIncorrect option! Please choose a number 1-8.");
             break;
         }
 
@@ -171,33 +96,6 @@ void matrixMenu()
     printf("\n\nGoodbye!\n\n");
 
     return 0;
-}
-
-
-//User Defined Function Definition
-void readMatrix(int array[10][10], int rows, int colums){
-    int i, j;
-    for (i = 0; i < rows; i++){
-        printf("\t%d entries for row %d: ", colums, i + 1);
-        for (j = 0; j < colums; j++){
-            scanf("%d", &array[i][j]);
-        }
-    }
-
-    return;
-}
-
-void printMatrix(int array[10][10], int rows, int colums){
-    int i, j;
-
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < colums; j++){
-            printf("\t%d", array[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 }
 
 #endif
