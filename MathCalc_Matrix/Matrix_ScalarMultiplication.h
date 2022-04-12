998f10b53450d99a1e07ecbe28910127e2afa617
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-12 19:11:09 +0400

    Modified Matrix Scalar Multiplication codes

--- MathCalc_Matrix/Matrix_ScalarMultiplication.h
+++ MathCalc_Matrix/Matrix_ScalarMultiplication.h
@@ -1,48 +1,42 @@
 #ifndef MATRIX_SCALAR_MULTIPLICATION_H_INCLUDED
-#define MATRIX_SCALAR_MULTIPLICATION_INCLUDED
-#include<stdio.h>
+#define MATRIX_SCALAR_MULTIPLICATION_H_INCLUDED
 
-//User Defined Function Declaration
-void readMatrix(int array[10][10], int rows, int colums);
-void printMatrix(int array[10][10], int rows, int colums);
-void matrixScalarMultiply(int array[10][10], int scalar, int rows, int colums);
-
-//User Defined Function Definition
-void readMatrix(int array[10][10], int rows, int colums){
-    int i, j;
-    for (i = 0; i < rows; i++){
-        printf("\t%d entries for row %d: ", colums, i + 1);
-        for (j = 0; j < colums; j++){
-            scanf("%d", &array[i][j]);
-        }
-    }
-
-    return;
+ 
+int scalarmultiplication()
+{
+ 	int i, j, rows, columns, Multiplication[10][10], Number;
+  
+ 	printf("\n Please Enter Number of rows and columns\n");
+ 	scanf("%d %d", &i, &j);
+ 
+ 	printf("\n Please Enter the Matrix Elements \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0;columns < j;columns++)
+    	{
+      		scanf("%d", &Multiplication[rows][columns]);
+    	}
+  	}
+   
+ 	printf("\n Please Enter the Multiplication Value  :  ");
+ 	scanf("%d", &Number);
+ 	  
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0; columns < j; columns++)
+    	{
+      		Multiplication[rows][columns] = Number * Multiplication[rows][columns];    
+   	 	}
+  	}
+ 
+ 	printf("\n The Result of a Scalar Matrix Multiplication is : \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0; columns < j; columns++)
+    	{
+      		printf("%d \t ", Multiplication[rows][columns]);
+    	}
+    	printf("\n");
+  	}
+ 	return 0;
 }
-
-void printMatrix(int array[10][10], int rows, int colums){
-    int i, j;
-
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < colums; j++){
-            printf("\t%d", array[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-void matrixScalarMultiply(int array[10][10], int scalar, int rows, int colums){
-    int i, j;
-    int scaM[10][10];
-    for (i = 0; i < rows; i++){
-        for (j = 0; j < colums; j++){
-            scaM[i][j] = scalar * array[i][j];
-            printf("%d\t", scaM[i][j]);
-        }
-        printf("\n");
-    }
-
-}
-
-#endif
