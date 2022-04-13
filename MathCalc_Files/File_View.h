127fd157acbd58fcffbb3465a861e23c1c339756
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-13 19:38:46 +0400

    cleared File_View.h

--- MathCalc_Files/File_View.h
+++ MathCalc_Files/File_View.h
@@ -1,67 +1,7 @@
 #ifndef FILE_VIEW_H_INCLUDED
 #define FILE_VIEW_H_INCLUDED
 
-int viewFile()
+void viewFile()
 {
-    int c,choice;
-    FILE * ptr;
-    FILE * ptr1;
-    FILE * ptr2;
-
-    printf("Which section's log file would you like to view:\n");
-    printf("1. Matrix operations\n2. Polynomial operations\n3. 2d and 3d shapes operations\n");
-    printf("4. Exit\n\n");
-    printf("Input your choice: ");
-    scanf("%d",&choice);
-
-    while(choice =! 4){
-        switch (choice)
-        {
-        case 1:
-            ptr=fopen("MathCalc_Matrix/Matrix_Log.txt","r");
-            if(ptr == NULL){
-                printf("File not found\n");
-            }
-            else{
-                printf("File opened in read mode\n\n");
-            } 
-            while((c=fgetc(ptr))!=EOF){
-                printf("%c",c);
-            }
-            break;
-        
-        case 2:
-            ptr=fopen("MathCalc_Polynomial/Polynomial_Log.txt","r");
-            if(ptr == NULL){
-                printf("File not found\n");
-            }
-            else{
-                printf("File opened in read mode\n\n");
-            } 
-            while((c=fgetc(ptr))!=EOF){
-                printf("%c",c);
-            } 
-            break;
-
-        case 3:
-            ptr=fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","r");
-            if(ptr == NULL){
-                printf("File not found\n");
-            }
-            else{
-                printf("File opened in read mode\n\n");
-            } 
-            while((c=fgetc(ptr))!=EOF){
-                printf("%c",c);
-            }
-            break;
-        
-        default:
-            printf("Wrong choice entered!\nInput again: ");
-            scanf("%d",&choice);
-            break;
-        }
-    }
-    return 0;
+    int c, choice;
 }
-#endif
