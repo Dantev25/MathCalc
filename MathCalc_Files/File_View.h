500ab6a8a126a2b79851c5abd05977b51d2102f6
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-13 15:52:27 +0400

    Created menu for File_View.h

--- MathCalc_Files/File_View.h
+++ MathCalc_Files/File_View.h
@@ -3,24 +3,68 @@
 
 int viewFile()
 {
+    int c,choice;
     FILE * ptr;
-    int c;
 
-    ptr=fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","r");
+    printf("Which section's log file would you like to view:\n");
+    printf("1. Matrix operations\n2. Polynomial operations\n3. 2d and 3d shapes operations\n");
+    printf("4. Exit");
+    printf("Input your choice: ");
+    scanf("%d",&choice);
 
-    if(ptr == NULL)
-    {
-        printf("File not found\n");
+    while(choice =! 4){
+        switch (choice)
+        {
+        case 1:
+            ptr=fopen("MathCalc_Matrix/Matrix_Log.txt","r");
+            if(ptr == NULL){
+                printf("File not found\n");
+            }
+            else{
+                printf("File opened in read mode\n\n");
+            } 
+            while((c=fgetc(ptr))!=EOF){
+                printf("%c",c);
+            }
+
+            fclose(fptr);
+            break;
+        
+        case 2:
+            ptr=fopen("MathCalc_Polynomial/Polynomial_Log.txt","r");
+            if(ptr == NULL){
+                printf("File not found\n");
+            }
+            else{
+                printf("File opened in read mode\n\n");
+            } 
+            while((c=fgetc(ptr))!=EOF){
+                printf("%c",c);
+            }
+
+            fclose(fptr);
+            break;
+
+        case 3:
+            ptr=fopen("MathCalc_Ar_Vol/Ar_Vol_Log.txt","r");
+            if(ptr == NULL){
+                printf("File not found\n");
+            }
+            else{
+                printf("File opened in read mode\n\n");
+            } 
+            while((c=fgetc(ptr))!=EOF){
+                printf("%c",c);
+            }
+
+            fclose(fptr);
+            break;
+        
+        default:
+            printf("Wrong choice entered!\nInput again: ");
+            scanf("%d",&choice);
+            break;
+        }
     }
-    else
-    {
-        printf("File opened in read mode\n\n");
-    } 
-    while((c=fgetc(ptr))!=EOF)
-    {
-        printf("%c",c);
-    }
-}
-//test
 
 #endif
