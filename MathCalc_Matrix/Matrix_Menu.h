33318aab1ae61cf8b0ccfde193f7e3180f4db967
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-13 20:35:56 +0400

    Updated Matrix Menu

--- MathCalc_Matrix/Matrix_Menu.h
+++ MathCalc_Matrix/Matrix_Menu.h
@@ -22,8 +22,8 @@ int matrixMenu()
     while (again == 'Y')
     {
 
-        printf("\nMatrix Operation Menu\n");
-        printf("\n\nPlease type in the number associated to the operation you wish to perform.\n");
+        printf("\n\t\tWelcome to the Matrix Operation Menu\n");
+        printf("\n\nPlease type in the number associated to the operation you wish to perform.\n\n");
         printf("\t1- Addition\n");
         printf("\t2- Subtraction\n");
         printf("\t3- Scalar Multiplication\n");
@@ -31,61 +31,70 @@ int matrixMenu()
         printf("\t5- Transpose\n");
         printf("\t6- Determinant\n");
         printf("\t7- Inverse\n");
-        printf("Enter your choice: ");
+        printf("\t8- View Matrix Log");
+        printf("\n\nEnter your choice: ");
         scanf(" %d", &operation);
 
 
         switch (operation){
 
-        case 1: 
-        
+        case 1:
+
+            printf("\n\n\t\tMatrix Addition\n\n");
             addition();
 
             break;
 
         case 2:
 
+            printf("\n\n\t\tMatrix Subtraction\n\n");
             subtraction();
             break;
 
         case 3:
 
+            printf("\n\n\t\tMatrix Scalar Multiplication\n\n");
             scalarmultiplication();
 
             break;
 
         case 4:
-            
+
+            printf("\n\n\t\tMatrix Multiplication\n\n");
             multiplication();
 
             break;
 
         case 5:
-            
+
+            printf("\n\n\t\tMatrix Transpose\n\n");
             transpose();
 
             break;
 
         case 6:
-            
+
+            printf("\n\n\t\tMatrix Determinant\n\n");
             determinant();
 
             break;
 
         case 7:
-            
+
+            printf("\n\n\t\tMatrix Inverse\n\n");
             inverse();
 
             break;
 
          case 8:
-            
+
+            printf("\n\n\t\tWelcome to Matrix Log\n\n");
             //log();
 
             break;
 
         default:
-            printf("\nIncorrect option! Please choose a number 1-8.");
+            printf("\nIncorrect option! Please choose a number 1-8.\n");
             break;
         }
 
