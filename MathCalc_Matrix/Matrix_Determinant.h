1ac26f5066f022af0672b7e04e5dc6eda97db369
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-14 15:34:24 +0400

    Tested Matrix Determinant and updated Matrix_Log.txt

--- MathCalc_Matrix/Matrix_Determinant.h
+++ MathCalc_Matrix/Matrix_Determinant.h
@@ -1,13 +1,16 @@
 #ifndef MATRIX_DETERMINANT_H_INCLUDED
 #define MATRIX_DETERMINANT_H_INCLUDED
+FILE *fptr;
 
 void printDmatrix(int a[10][10], int r, int c){
     int i, j;
 
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++){
+			fprintf(fptr,"\t%d", a[i][j]);
             printf("\t%d", a[i][j]);
         }
+		fprintf(fptr, "\n\n");
         printf("\n\n");
     }
 }  
@@ -15,6 +18,18 @@ int determinant()
 {
 	 int a[10][10], x[10], ratio, det=1;
 	 int i,j,k,n;
+	 time_t t;   
+     time(&t);
+
+	 fptr = (fopen("MathCalc_Matrix/Matrix_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
+    }
+
+    fprintf(fptr,"Executed on: %s",ctime(&t));
+    fprintf(fptr,"\nMatrix Operation : Determinant\n");
 
 	 printf("\nEnter Order of Matrix: ");
 	 scanf("%d", &n);
@@ -29,6 +44,7 @@ int determinant()
 			   scanf("%d", &a[i][j]);
 		  }
 	 }
+	fprintf(fptr, "\nInput:\n\n"); 
 	printf("\nThe Matrix is \n\n");
 	printDmatrix(a,i,j);
 
@@ -37,9 +53,11 @@ int determinant()
      {
          det = det * a[i][i];
      }
+	 fprintf(fptr, "\nOutput: ");
+	 printf("\n\nDeterminant of the Matrix is %d. \n", det);
+	 fprintf(fptr, "\n\nDeterminant of the Matrix is %d. \n", det);
 
-	 printf("\n\nDeterminant of the Matrix is: %d \n", det);
-
+	fclose(fptr);
 
 	 return 0;
 }
