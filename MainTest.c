961e5f9717d8c007cc63209ce3bcbd5e91fd6617
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-14 14:01:57 +0400

    Tested Matrix Addition and updated Matrix_Log.txt

--- MainTest.c
+++ MainTest.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<math.h>
+#include <time.h>
 #include"MathCalc_Ar_Vol\Ar_Vol_Menu.h"
 #include"MathCalc_Matrix\Matrix_Menu.h"
 
--- MathCalc_Matrix/Matrix_Addition.h
+++ MathCalc_Matrix/Matrix_Addition.h
@@ -1,26 +1,34 @@
 #ifndef MATRIX_ADDITION_H_INCLUDED
 #define MATRIX_ADDITION_H_INCLUDED
+FILE *fptr;
 
 void printMatrix(int a[10][10], int r, int c){
     int i, j;
 
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++){
+            fprintf(fptr,"\t%d", a[i][j]);
             printf("\t%d", a[i][j]);
         }
+        fprintf(fptr, "\n\n");
         printf("\n\n");
     }
 }
 
 int addition() {
   int r, c, a[10][10], b[10][10], sum[10][10], i, j;
+  time_t t;   // not a primitive datatype
+  time(&t);
+
+  
+
   printf("Enter the number of rows : ");
   scanf("%d", &r);
   //verifying if r is valid
    while(r <= 0)
    {
         printf("\nYou cannot have 0 or negative number of rows.\n Please input a positive integer not exceeding 10 : ");
-        scanf("%f",&r);
+        scanf("%d",&r);
     }
 
 
@@ -31,9 +39,17 @@ int addition() {
    while(c <= 0)
    {
         printf("\nYou cannot have 0 or negative number of columns.\nPlease input a positive integer not exceeding 10 : ");
-        scanf("%f",&c);
+        scanf("%d",&c);
+    }
+      fptr = (fopen("MathCalc_Matrix/Matrix_Log.txt","a"));
+
+    if(fptr==NULL){
+        printf("Error!");
+        exit(1);
     }
 
+    fprintf(fptr,"Executed on: %s",ctime(&t));
+    fprintf(fptr,"Matrix Operation : Addition\n");
 
   printf("\nEnter elements of 1st matrix:\n\n");
   for (i = 0; i < r; ++i)
@@ -41,9 +57,9 @@ int addition() {
       printf("Enter element a%d%d: ", i + 1, j + 1);
       scanf("%d", &a[i][j]);
     }
-
+  fprintf(fptr, "\nInput:\nFirst Matrix:\n\n");
   printf("\nThe First Matrix is \n\n");
-    printMatrix(a,r,c);
+  printMatrix(a,r,c);
 
   printf("Enter elements of 2nd matrix:\n\n");
   for (i = 0; i < r; ++i)
@@ -51,7 +67,7 @@ int addition() {
       printf("Enter element b%d%d: ", i + 1, j + 1);
       scanf("%d", &b[i][j]);
     }
-
+  fprintf(fptr, "\nInput:\nSecond Matrix:\n\n");
   printf("\nThe Second Matrix is \n\n");
   printMatrix(b,r,c);
 
@@ -62,14 +78,18 @@ int addition() {
     }
 
   // printing the result
-  printf("\nThe Sum of two matrices is \n\n");
+  fprintf(fptr, "\nOutput: \n\n");
+  printf("\nThe Sum of two matrices is: \n\n");
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
+      fprintf(fptr, "\t%d   ", sum[i][j]);
       printf("\t%d   ", sum[i][j]);
       if (j == c - 1) {
+        fprintf(fptr, "\n\n");
         printf("\n\n");
       }
     }
+fclose(fptr);
 
   return 0;
 }
