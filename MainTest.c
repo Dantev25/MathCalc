69b71cddb8415d08e58890dfff4ba9316584680e
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-14 21:48:29 +0400

    Tested for viewFile in main.

--- MainTest.c
+++ MainTest.c
@@ -5,10 +5,15 @@
 #include <time.h>
 #include"MathCalc_Ar_Vol\Ar_Vol_Menu.h"
 #include"MathCalc_Matrix\Matrix_Menu.h"
+#include"MathCalc_Files\File_View.h"
 
 
 
 int main()
 {
+<<<<<<< Updated upstream
      MatrixMenu();
+=======
+     viewFile();
+>>>>>>> Stashed changes
 }
