7574d078350552d8218cb20e1866547fea036c82
Sadiyahk25 <sadiyahkholeepa@gmail.com>
2022-04-16 23:25:16 +0400

    Added log file 'read' codes for complex numbers in File_View.h.

--- MathCalc_Files/File_View.h
+++ MathCalc_Files/File_View.h
@@ -10,7 +10,7 @@ int viewFile()
     printf("\n\t\tWelcome to Log Selection Menu\n");
     while(again == 'Y'){
         printf("\n\nPlease type in the number associated to the log file you wish to view.\n\n");
-        printf("\t1- Matrix Operations\n\t2- Polynomial Operations\n\t3- 2D AND 3D Shapes Operations");
+        printf("\t1- Matrix Operations\n\t2- Polynomial Operations\n\t3- 2D AND 3D Shapes Operations\n\t4- Complex Numbers Operations");
         printf("\n\nInput your choice: ");
         scanf("%d",&choice);
 
@@ -59,11 +59,26 @@ int viewFile()
             fclose(fptr);
         }
 
-        else{
-            printf("\nIncorrect Input!\nPlease choose a number between 1-3!");
+        else if(choice == 4){
+            fptr = fopen("MathCalc_Complex/Complex_Log.txt","r");
+            if(fptr == NULL){
+                printf("\nFile not found\n");
+            }
+            else{
+                printf("\nFile opened in read mode\n\n");
+            } 
+            while((c = fgetc(fptr)) != EOF){
+                printf("%c",c);
+            }
+            printf("\n\n");
+            fclose(fptr);
         }
 
-        printf("\n\nDo you want to view a Log File again? Y/N\n");
+        else{
+            printf("\nIncorrect Input!\nPlease choose a number between 1-4!");
+        }
+
+        printf("\nDo you want to view a Log File again? Y/N\n");
         scanf(" %c", &again);
         again = toupper(again);
     }
