6f633f462f3b2a4774664a969bc43b27a8aa9339
Mrishana Rajanah <mrishanar2712@gmail.com>
2022-04-16 19:50:53 +0400

    Cleared Main Test.

--- MainTest.c
+++ MainTest.c
@@ -6,10 +6,9 @@
 
 #include"MathCalc_Ar_Vol\Ar_Vol_Menu.h"
 #include"MathCalc_Polynomial\Polynomial_Division.h"
-#include"MathCalc_Matrix\Matrix_Menu.h"
-#include"MathCalc_Files\File_View.h"
+
 
 int main()
 {
-     MatrixMenu();
+     
 }
